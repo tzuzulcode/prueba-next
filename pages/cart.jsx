3b6f75061c34d670c9f84ab64f9c64110724977b
Tzuzul Code <mail@tzuzulcode.com>
2022-02-04 20:55:07 -0600

    Correción de componentes

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Products from '../components/Products';
 
-export default function cart() {
+export default function Cart() {
     const {items} = useSelector(state=>state.cart)
   return <>
       <section>
