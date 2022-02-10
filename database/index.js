f702c51095c965f3cccd33f6833a0840c01cae71
Tzuzul Code <mail@tzuzulcode.com>
2022-02-09 21:13:21 -0600

    Instalación de firebase ui y corrección de detalles

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 import { initializeApp,getApps,getApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 let app
 
